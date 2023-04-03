39d24b0286265c666a53d9becb2acf43fb85671d
GCRusso <gabriel.coral.russo@gmail.com>
2023-04-02 22:17:34 -0300

    exercicios

---
+++ exercicio 2/Program.cs
@@ -0,0 +1,41 @@
+﻿
+// o maior, menor e o numero esta = 0, so para criar a variavel poderia utilizar qualquer numero que seria o mesmo efeito. TL = 4 pois informa a quantidade de vezes que ira fazer o loop do codigo no caso 4 vezes.
+
+int maior = 0, menor = 0, i = 0, numero = 0, tl = 4;
+
+//cria a primeira inserção de numero do usuario
+Console.WriteLine($"Informe o 1 número: ");
+numero = Convert.ToInt32(Console.ReadLine());
+maior = numero;
+menor = numero;
+// o i esta representando nosso contador, aqui esta avisando que ja recebemos o primeiro numero.
+i = 1;
+
+//o i (contador) aqui ja esta em 2 pois sera o segundo codigo, <=tl é o valor que foi mencionado de quantas vezes sera o loop q no caso foi 4vezes pois sera 4 numeros, i++ é o incremento do contador, estamos em 2 o proximo sera 3 e depois 4 que chega ao fim do loop.
+for (i= 2; i <= tl; i++)
+{
+    //o contador aqui nos ajuda a alterar a nossa frase para o usuario, pedindo para informar o 1,2,3 e 4.
+    Console.WriteLine($"Informe o " + i + " número: ");
+    numero = Convert.ToInt32(Console.ReadLine());
+
+    // se o numero que usuario informou for menor que meu atual menor, o meu atual menor recebera o valor de "numero"
+    if (numero < menor)
+    {
+        menor = numero;
+    }
+
+    // se o numero que o usuario informou for maior que meu atual maior, o meu atual maior recebera o valor de "numero"
+    if (numero > maior)
+    {
+        maior = numero;
+    }
+}
+
+//aqui forma a mensagem mostrando para o usuario qual foi o maior e o menor numero.
+Console.WriteLine($"O maior número é: {maior}");
+Console.WriteLine($"O menor número é: {menor}");
+
+
+
+
+
---
+++ exercicio/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.WriteLine($"Qual o dia em que você nasceu?: ");
+int dia = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"Qual o mês em que você nasceu?: ");
+int mes = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"Qual o ano em que você nasceu?: ");
+int ano = int.Parse(Console.ReadLine());
+
+
+ 
+if (dia > 31 )
+{
+Console.WriteLine($"Data Invalida! coloque entre 1 e 31." );
+
+if (mes > 12)
+Console.WriteLine($"Mes Invalido! Coloque entre 1 e 12." );
+
+}
+
+else if (ano > 2013)
+Console.WriteLine($"Ano invalido! Coloque abaixo do ano em que estamos!" );
+
+else {
+    Console.WriteLine($"Você nasceu no dia {dia}, no mês {mes} e ano {ano} ");
+    
+}
