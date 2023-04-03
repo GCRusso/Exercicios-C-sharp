14f00c012da83fd6bfe03d3bc308957a75074bba
Gabriel Coral Russo <23655858841@info.local>
2023-04-03 08:42:27 -0300

    exercicio 3

--- exercicio 2/Program.cs
+++ exercicio 2/Program.cs
@@ -1,11 +1,11 @@
 ﻿
 // o maior, menor e o numero esta = 0, so para criar a variavel poderia utilizar qualquer numero que seria o mesmo efeito. TL = 4 pois informa a quantidade de vezes que ira fazer o loop do codigo no caso 4 vezes.
 
-int maior = 0, menor = 0, i = 0, numero = 0, tl = 4;
+int maior = 0, menor = 0, i = 0, numero = 0, tl = 10;
 
 //cria a primeira inserção de numero do usuario
 Console.WriteLine($"Informe o 1 número: ");
-numero = Convert.ToInt32(Console.ReadLine());
+numero = int.Parse (Console.ReadLine());
 maior = numero;
 menor = numero;
 // o i esta representando nosso contador, aqui esta avisando que ja recebemos o primeiro numero.
@@ -16,7 +16,7 @@ for (i= 2; i <= tl; i++)
 {
     //o contador aqui nos ajuda a alterar a nossa frase para o usuario, pedindo para informar o 1,2,3 e 4.
     Console.WriteLine($"Informe o " + i + " número: ");
-    numero = Convert.ToInt32(Console.ReadLine());
+    numero = int.Parse (Console.ReadLine());
 
     // se o numero que usuario informou for menor que meu atual menor, o meu atual menor recebera o valor de "numero"
     if (numero < menor)
