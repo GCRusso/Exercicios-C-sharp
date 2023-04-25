071f9e838eddd66278076bd5b7ff6bd65b3fb00e
GCRusso <gabriel.coral.russo@gmail.com>
2023-04-25 11:37:56 -0300

    inicio do projeto Celular 25.04

---
+++ Exercicio 25.04 - celular/Celular.cs
@@ -0,0 +1,30 @@
+namespace Exercicio_25._04___celular
+{
+    public class Celular
+    {
+        //Cadastro do celular
+        public void CadastroCelular()
+        {
+            Console.WriteLine($"***** Bem vindo ao CadastraPhone *****");
+            Console.WriteLine($"");
+            Console.WriteLine($"Qual o modelo do celular?: ");
+            string modelo = Console.ReadLine();
+            Console.WriteLine($"Qual a cor do celular?: ");
+            string cor = Console.ReadLine();
+            Console.WriteLine(@$"Qual o tamanho do celular?: ");
+            string tamanho = Console.ReadLine();
+            Console.WriteLine($"");
+            Console.WriteLine($"Celular Cadastrado com sucesso!!!");
+        
+        }
+
+//Menu o que deseja fazer
+
+
+public void LigarCelular()
+{
+
+}
+
+    }
+}
---
+++ Exercicio 25.04 - celular/Program.cs
@@ -0,0 +1,38 @@
+﻿/*Crie uma Classe de um Celular, com as propriedades cor, modelo, tamanho, ligado(booleano).
+Com os métodos, ligar, desligar, fazer ligação, enviar mensagem.
+Só será possível executar tais métodos se o celular estiver ligado.
+Envie o link do repositório como entrega desta atividade.
+
+1- propriedades da classe - cor, modelo e tamanho
+3- se estiver ligado
+2- metodos - ligar, desligar, fazer ligação e enviar mensagem 
+*/
+
+using Exercicio_25._04___celular;
+
+Celular a = new Celular();
+
+a.CadastroCelular();
+
+Console.WriteLine(@$"
+Funções do celular
+1- Ligar o celular
+2- Desligar o celular
+
+0- Sair
+");
+char opcao = char.Parse(Console.ReadLine());
+
+switch (opcao)
+{
+    case '1':
+        break;
+
+    case '2':
+        break;
+
+    default:
+        break;
+}
+
+
